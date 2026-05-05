3ac096eba722078bc1aa42ef6f0dc49134ab791c
Shifa <shifachugh06@gmail.com>
2026-05-05 18:36:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+// console.log("Chai or Code")
+const express = require('express')
+// or
+// import express from "express"
+
+const app = express()  // abb app. kar ke bhot kuch use kar skte hai
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+    res.send('shifa.com')
+})
+app.get('/login', (req, res) => {
+    res.send("<h1>please login at shifa's first website to deploy</h1>")
+})
+app.get('/youtube', (req, res) => {
+    res.send("<h2>Learing from ChaiAurCode</h2>")
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
